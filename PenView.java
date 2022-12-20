4ba071d72d37d8ec5fc39bdf9c8644a6a7d8cd2e
Priiyyya-21 <priyamajhi79@gmail.com>
2022-12-21 00:30:21 +0530

    Added View Class

---
+++ PenView.java
@@ -0,0 +1,12 @@
+public class PenView {
+
+    void printPenDetails(String name, String brand, double price, String color) {
+
+        System.out.println("Pen name: " + name);
+        System.out.println("Pen brand: " + brand);
+        System.out.println("Pen price: ₹" + price);
+        System.out.println("Pen color: " + color);
+
+    }
+
+}
