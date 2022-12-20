6d332168e37125059c2b247370e7a08afc94b301
Priiyyya-21 <priyamajhi79@gmail.com>
2022-12-21 00:29:24 +0530

    added model class

---
+++ PenModel.java
@@ -0,0 +1,41 @@
+class PenModel {
+
+    private String name;
+    private String brand;
+    private double price;
+    private String color;
+
+    static int quantity = 100;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
